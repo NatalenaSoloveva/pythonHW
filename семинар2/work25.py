1b7051fa3e9d3f5e849a3e9b53c8da20300095b9
Nataly <ahsatan84@mail.ru>
2023-01-02 19:34:24 +0300

    исправила  work 25

--- семинар2/work25.py
+++ семинар2/work25.py
@@ -4,4 +4,4 @@ import random
 lst = [random.randint(0, 10) for i in range(random.randint(5, 20))]
 print(f"исходный список:\n {lst}")
 random.shuffle(lst)
-print(f"список после перемешивания:\n{lst}"
+print(f"список после перемешивания:\n{lst}")
